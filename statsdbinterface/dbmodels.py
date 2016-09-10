2e96258d79e76a8451f9719c95a448566960e1c9
Beha <shacknetisp@mail.com>
2016-09-10 14:33:05 -0400

    Add models for the remaining tables.

--- statsdbinterface/dbmodels.py
+++ statsdbinterface/dbmodels.py
@@ -1,6 +1,80 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy import and_
 from statsdbinterface.database import db
-from statsdbinterface.modelutils import direct_to_dict
+from statsdbinterface.modelutils import direct_to_dict, list_to_id_dict
+
+
+class GameBombing(db.Model):
+    __tablename__ = "game_bombings"
+
+    rowid = db.Column(db.Integer, primary_key=True)
+    game = db.Column(db.Integer)
+    player = db.Column(db.Integer)
+    playerhandle = db.Column(db.Text)
+    bombing = db.Column(db.Integer)
+    bombed = db.Column(db.Integer)
+
+    def to_dict(self):
+        return direct_to_dict(self, [
+            "game", "player", "playerhandle",
+            "bombing", "bombed"
+        ])
+
+
+class GameCapture(db.Model):
+    __tablename__ = "game_captures"
+
+    rowid = db.Column(db.Integer, primary_key=True)
+    game = db.Column(db.Integer)
+    player = db.Column(db.Integer)
+    playerhandle = db.Column(db.Text)
+    capturing = db.Column(db.Integer)
+    captured = db.Column(db.Integer)
+
+    def to_dict(self):
+        return direct_to_dict(self, [
+            "game", "player", "playerhandle",
+            "capturing", "captured"
+        ])
+
+
+class GameTeam(db.Model):
+    __tablename__ = "game_teams"
+
+    game_id = db.Column('game',
+        db.Integer, db.ForeignKey('games.id'), primary_key=True)
+    game = db.relationship('Game',
+        backref=db.backref('teams', lazy='dynamic'))
+    team = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    name = db.Column(db.Text)
+
+    def to_dict(self):
+        return direct_to_dict(self, [
+            "game_id", "team", "score", "name"
+            ])
+
+
+class GameFFARound(db.Model):
+    __tablename__ = "game_ffarounds"
+
+    game_id = db.Column('game',
+        db.Integer, db.ForeignKey('games.id'), primary_key=True)
+    game = db.relationship('Game',
+        backref=db.backref('ffarounds', lazy='dynamic'))
+    player = db.Column(db.Integer, primary_key=True)
+    playerhandle = db.Column(db.Text)
+    round = db.Column(db.Integer, primary_key=True)
+    winner = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<GameFFARound %d from %d (%d)>' % (self.round,
+            self.game_id, self.player)
+
+    def to_dict(self):
+        return direct_to_dict(self, [
+            "game_id", "player", "playerhandle", "round", "winner"
+            ])
 
 
 class Game(db.Model):
@@ -18,6 +92,21 @@ class Game(db.Model):
     def __repr__(self):
         return '<Game %d>' % (self.id)
 
+    def combined_ffarounds(self):
+        # Combine the various game_ffarounds entries into a single list.
+        ffarounds = {}
+        for ffaround in self.ffarounds:
+            index = ffaround.round
+            if index not in ffarounds:
+                ffarounds[index] = {
+                    "winner": None,
+                    "players": [],
+                }
+            if ffaround.winner:
+                ffarounds[index]["winner"] = ffaround.player
+            ffarounds[index]["players"].append(ffaround.player)
+        return ffarounds
+
     def to_dict(self):
         return direct_to_dict(self, [
                 "id", "time",
@@ -26,7 +115,12 @@ class Game(db.Model):
                 "usetotals"
             ],
             {
-                "players": [p.to_dict() for p in self.players]
+                "teams": list_to_id_dict([t.to_dict() for t in self.teams],
+                    "team"),
+                "players": list_to_id_dict([p.to_dict() for p in self.players],
+                    "wid"),
+                "ffarounds": self.combined_ffarounds(),
+                "server": self.server.first().to_dict(),
             })
 
 
@@ -50,8 +144,44 @@ class GamePlayer(db.Model):
         return '<GamePlayer %d from %d (%s [%s])>' % (
             self.wid, self.game_id, self.name, self.handle)
 
+    def bombings(self):
+        return [b.to_dict() for b in GameBombing.query.filter(
+            and_(GameBombing.game == self.game_id,
+                GameBombing.player == self.wid)).all()]
+
+    def captures(self):
+        return [b.to_dict() for b in GameCapture.query.filter(
+            and_(GameCapture.game == self.game_id,
+                GameCapture.player == self.wid)).all()]
+
     def to_dict(self):
         return direct_to_dict(self, [
             "game_id", "name", "handle",
             "score", "timealive", "frags", "deaths", "wid", "timeactive"
+            ], {
+                "bombings": self.bombings(),
+                "captures": self.captures(),
+            })
+
+
+class GameServer(db.Model):
+    __tablename__ = 'game_servers'
+
+    game_id = db.Column('game',
+        db.Integer, db.ForeignKey('games.id'), primary_key=True)
+    game = db.relationship('Game',
+        backref=db.backref('server', lazy='dynamic'))
+    handle = db.Column(db.Text)
+    flags = db.Column(db.Text)
+    desc = db.Column(db.Text)
+    version = db.Column(db.Text)
+    host = db.Column(db.Text)
+    port = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<GameServer %s from %d)>' % (self.handle, self.game_id)
+
+    def to_dict(self):
+        return direct_to_dict(self, [
+            "game_id", "handle", "flags", "desc", "version", "host", "port"
             ])
--- statsdbinterface/modelutils.py
+++ statsdbinterface/modelutils.py
@@ -14,3 +14,11 @@ def direct_to_dict(base, attributes, update=None):
     if update:
         ret.update(update)
     return ret
+
+
+# Return a dict from an attribute in a list of dicts.
+def list_to_id_dict(base, attribute):
+    ret = {}
+    for d in base:
+        ret[d[attribute]] = d
+    return ret
